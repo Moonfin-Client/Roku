fc83a1fb9e7c2dee56df7e07f7aa0558fb2c2893
Axl Nunez <103554043+RadicalMuffinMan@users.noreply.github.com>
2026-08-27 12:42:47 -0400

    Deduplicate Roku locale files

--- locale/ar/translations.ts
+++ locale/ar/translations.ts
@@ -1675,10 +1675,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Skip</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Sort by</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1804,10 +1800,6 @@
     </message>
     <message>
         <source>Manual Login</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Unhide Users</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
